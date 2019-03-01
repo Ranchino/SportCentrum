1908d6f73169b212f1592ea8671d2271730a318f
Ranj Bahadin <ranjbahadin@live.se>
2019-03-01 11:32:30 +0100

    adding ajax

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Document</title>
 </head>
-<body>
-<!-- bbb -->
-<!-- bbbb2 
-    ddd
+<body >
+    <button type="button" >Testar</button>
 </body>
+    <script scr="./ajaxscript.js"></script>
 </html>
