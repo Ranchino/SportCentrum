a0c2116a05ac8097cee56e13ae567a333d62016e
SakineMazlomyar <mazlomyar12@gmail.com>
2019-03-01 15:11:53 +0100

    accepted

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Document</title>
-</head>
-<<<<<<< HEAD
-<body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <!-- this is a new test -->    
-=======
-    <!--Ahmed sugere -->
->>>>>>> 8b069c27ffb057645eb3d7fc3f16af67e834fba6
-=======
-
-    <?php 
-        include 'header.php';
-    ?>
-
-
-    <!-- test for developbranch -->
-    
->>>>>>> da4f14b01c3423522fcc6315876fd0c24c1e9390
-=======
-<body >
-    <button type="button" >Testar</button>
->>>>>>> 1908d6f73169b212f1592ea8671d2271730a318f
-=======
 <html>
 <title>SportCentrum</title>
 <meta charset="UTF-8">
