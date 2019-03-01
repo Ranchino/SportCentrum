35162588e1ea96f3d3de25df9d30731e0adc5d76
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-01 11:33:51 +0100

    creating template, creating js

--- php-files/header.php
+++ php-files/header.php
@@ -1,23 +1,26 @@
-<div class="container-for-navbar">
-  <div class="ui menu">
-    <div class="header item">
-    SportCentrum
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+  <div class="w3-container w3-display-container w3-padding-16">
+    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+    <h3 class="w3-wide"><b>SportCentrum</b></h3>
   </div>
-  <a class="item">
-    About Us
-  </a>
-  <a class="item">
-    Jobs
-  </a>
-  <a class="item">
-    Locations
-  </a>
-    <div class="item">
-      <div class="ui primary button">Sign up</div>
-    </div>
-    <div class="item">
-      <div class="ui button">Log-in</div>
-    </div>
-  </div>
-</div>
+  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+    <a href="index.php" class="w3-bar-item w3-button">Women</a>
+    <a href="mensSite.php" class="w3-bar-item w3-button">Men</a>
+    <a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">
+      Children
+    </a>
 
+  <a href="#footer" class="w3-bar-item w3-button w3-padding">Contact</a> 
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding" onclick="document.getElementById('newsletter').style.display='block'">Newsletter</a> 
+  <a href="#footer"  class="w3-bar-item w3-button w3-padding">Subscribe</a>
+</nav>
+
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">SportCentrum</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
