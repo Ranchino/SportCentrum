179de04f88449bc15b251d39b63d481da7269850
SakineMazlomyar <mazlomyar12@gmail.com>
2019-03-04 10:27:25 +0100

    finishedLogin

--- Api/ajaxscript.js
+++ Api/ajaxscript.js
@@ -6,11 +6,30 @@ function test(){
         url: './Handler/requestHandler.php',
         data: test, 
         success: function(data) {
-            console.log(data);
+            alert(data)
         },
         error: function checkerror(error){
             console.log(error);
         }
     });
     return false;
+}
+
+
+function registerNewUser() {
+    var registerForm = $("#registering").serialize();
+    $.ajax({
+        method: 'POST',
+        datatype: 'json',
+        url: './Handler/requestHandler.php',
+        data: registerForm, 
+        success: function(data) {
+            console.log(data)
+        },
+        error: function checkerror(error){
+            console.log(error);
+        }
+    });
+    return false;
+
 }
--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +12,43 @@
     <title>Document</title>
 </head>
 <body >
-    
+<!-- login here user/admin -->
 <form id="info">
-        <h1>Log in Here</h1><br>
+        <h1>Log in Here user/admin</h1><br>
+        <label>Email </label><br>
         <input type="text" name="username"><br>
+        <label>Password </label><br>
         <input type="text" name="password"><br>
         <input type="submit"  name="clickedButton" onclick="return test()" value="click here">
-    </form>
+</form><br>
+<!-- Register new user here -->
+<!-- <form id="registering">
+        <h1>Register in Here New User </h1><br>
+        <label>Mail</label><br>
+        <input type="text" name="mail"><br>
+        <label>Password</label><br>
+        <input type="text" name="passwordRegister"><br>
+        <label>Vill ha nyhetsbrev</label><br>
+        <select name="nyhetsbrev"><br>
+            <option value="ja">Ja</option>
+            <option value="nej">Nej</option>
+        </select><br>
+        <input type="submit"  name="isClicked" onclick="return registerNewUser()" value="Register me">
+</form> -->
 
+<?php
+//Working on redirect
+/* if(isset($_SESSION['rol'])){
+    if($_SESSION['rol'] == 'Admin') {
+        header("Location: adminSite.php");
+        die;
+    } 
+    if($_SESSION['rol'] == 'user'){
+        header("Location: index.php");
+        die;
+    }
+    
+} */
+?>
 </body>
 </html>
