24302a733741c42fdc25da0b18f732a36bae6b00
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-07 09:54:44 +0100

    push newsletter

--- view/footer.php
+++ view/footer.php
@@ -64,7 +64,10 @@
       <i onclick="document.getElementById('newsletter').style.display='none'" class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
       <h2 class="w3-wide">NEWSLETTER</h2>
       <p>Join our mailing list to receive updates on new arrivals and special offers.</p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Surname"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Lastname"></p>
       <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Phonenumber"></p>
       <button type="button" class="w3-button w3-padding-large w3-red w3-margin-bottom" onclick="document.getElementById('newsletter').style.display='none'">Subscribe</button>
     </div>
   </div>
