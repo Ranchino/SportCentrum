3e1b9a711256e530eab145e1632b70b8ff5f9c3c
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-08 22:03:12 +0100

    push

--- view/userPage.php
+++ view/userPage.php
@@ -5,13 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css-files/styleSheet.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./Scripts/productPage.js"></script>
+    <script src="./Scripts/loginRegister.js"></script>
+
     <title>Document</title>
+
 </head>
+
 <body>
-    <p>Välommen till user sida!</p>
-    <form method="post">
-        <input type="submit"  name="isClicked" value="Go back!">
+
+    <header class="header-for-userPage">
+    <p style="color: white; text-align: center;">Welcome!</p>
+    <form  class=" user-page-form" method="post">
+        <input style =" border-radius: 0.5em; background-color: black; color: white;" type="submit"  name="isClicked" value="Go back!">
     </form>
+    </header>
+
+    <div class="user-side-bar">
+        <ul class="unordered-list">
+            <li style="padding: 1em;">Number of orders : </li>
+            <li style="padding: 1em;">Recieved Orders : </li>
+            <li style="padding: 1em;">Shipping Methods : </li>
+            <li style="padding: 1em;">Payment Action : </li>
+            <li style="padding: 1em;">Change Shipping Adress : </li>
+        </ul>
+    </div>
+
     <?php
 
     if(isset($_POST['isClicked'])){
