2b6631b578994c15c854631d5203784903b7087d
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-11 11:22:33 +0100

    creating css files and styling

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="./css-files/styleSheet.css">
+<link rel="stylesheet" href="./css-files/style-mobile.css">
+<link rel="stylesheet" href="./css-files/style-tablet.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="./Scripts/productPage.js"></script>
 <script src="./Scripts/w3-school.js"></script>
