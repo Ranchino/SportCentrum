18d7af1e229c0c96d9907c9b9bf1715684361fe9
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-15 12:03:21 +0100

    push

--- view/shoppingCart.php
+++ view/shoppingCart.php
@@ -144,7 +144,7 @@
             <input type="text" id="lname" name="lastname" placeholder="Svensson">
             <label for="email"><i class="fa fa-envelope"></i> Email</label>
             <input type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+            <label for="adr"><i class="fa fa-home"></i> Address</label>
             <input type="text" id="adr" name="address" placeholder="Svenssongatan 14a">
             <label for="city"><i class="fa fa-institution"></i> City</label>
             <input type="text" id="city" name="city" placeholder="Göteborg">
