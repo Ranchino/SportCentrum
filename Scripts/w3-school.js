95cdddd60d8d3097419f809d1da960750f8172c2
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-15 10:48:13 +0100

    push

--- Scripts/w3-school.js
+++ Scripts/w3-school.js
@@ -39,6 +39,16 @@ function specialTreatmentVip() {
   }
 }
 
+// Get the modal
+var modal = document.getElementById('id09');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
   // Get the modal
 var modal = document.getElementById('id01');
 
