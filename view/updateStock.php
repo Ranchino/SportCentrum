6e140e74968b8dd0f346f8a2c53a97b8f8bcd17e
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-15 13:02:28 +0100

    push

--- view/updateStock.php
+++ view/updateStock.php
@@ -10,14 +10,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Update unit in stock</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css-files/updateStockMobile.css">
+    <link rel="stylesheet" href="../css-files/updateStockDesktop.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="../Scripts/updateStock.js"></script>
 
 </head>
+
+
 <body>
+
+    
     <br>
     <br>
+    <a href="../view/adminPage.php">Go Back</a>
     <button onclick=listAllProducts()>List of all Products</button>
     <br>
     <div id="tempForm" style='text-align:center'> 
